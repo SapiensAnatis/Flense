4f75c0075670b7b26359610d9bd443556343d18d
Jay <jay0@me.com>
2026-08-25 22:04:22 +0100

    Fix enter script

--- Scripts/enter-claude-container.ps1
+++ Scripts/enter-claude-container.ps1
@@ -10,5 +10,13 @@ $ErrorActionPreference = "Stop"
 $composeFile = "$PSScriptRoot\..\Container\docker-compose.yaml"
 
 docker compose -f "$composeFile" up -d
-docker compose -f "$composeFile" exec -u ContainerAdministrator dev icacls C:\Users\ContainerUser\.claude /grant 'ContainerUser:(OI)(CI)M' /T /Q  # The mounted volume has the wrong permissions by default
+
+
+$volumeMounts = @("C:\Users\ContainerUser\.claude", "C:\Users\ContainerUser\.nuget\packages")
+
+# The mounted volumes have the wrong permissions by default
+foreach ($volumeMount in $volumeMounts) {
+    docker compose -f "$composeFile" exec -u ContainerAdministrator dev icacls $volumeMount /grant 'ContainerUser:(OI)(CI)M' /T /Q 
+}
+
 docker compose -f "$composeFile" exec dev claude
